244e37725c3168a913e3197794f0abadbdb83ff6
Nishanth0203 <dugyalanishanthrao@gmail.com>
2022-12-25 16:45:30 +0530

    Updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,12 @@
-// require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-waffle")
 // require("hardhat-gas-reporter")
 // require("@nomiclabs/hardhat-etherscan")
 // require("dotenv").config()
 // require("solidity-coverage")
 require("hardhat-deploy")
+
+// const { waffle } = require("hardhat");
+// const { deployContract } = waffle;
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 /**
@@ -11,19 +14,13 @@ require("hardhat-deploy")
  */
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
-const KOVAN_RPC_URL =
-    process.env.KOVAN_RPC_URL ||
-    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const RINKEBY_RPC_URL =
-    process.env.RINKEBY_RPC_URL ||
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL ||
     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY =
     process.env.PRIVATE_KEY ||
     "0x7c5b8fde30b56387e9eca4e958fefb28c93561f2639f672bcbdc2a8a4f600426"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
-// const GOERLI_RPC_URL = 
-//     procees.env.GOERLI_RPC_URL || 
-//     "https://eth-goerli.g.alchemy.com/v2/BIUhUGSSQYE6n4Tlm3e5n7U6rJG5RuA_"
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -32,38 +29,27 @@ module.exports = {
             chainId: 31337,
             // gasPrice: 130000000000,
         },
-        kovan: {
-            url: KOVAN_RPC_URL,
+        goerli: {
+            url: GOERLI_RPC_URL,
             accounts: [PRIVATE_KEY],
-            chainId: 42,
-            blockConfirmations: 6,
-            gas: 6000000,
-        },
-        rinkeby: {
-            url: RINKEBY_RPC_URL,
-            accounts: [PRIVATE_KEY],
-            chainId: 4,
+            chainId: 5,
             blockConfirmations: 6,
         },
-        // goerli: {
-        //   url: GOERLI_RPC_URL,
-        //   accounts: [PRIVATE_KEY],
-        //   chainId: 5,
-        //   blockConfirmations: 6,
-        // }
     },
     solidity: {
         compilers: [
             {
-                version: "0.8.8",
+                version: "0.8.7",
             },
             {
-                version: "0.6.6",
+                version: "0.8.7",
             },
         ],
     },
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+        // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
+
     },
     gasReporter: {
         enabled: true,
@@ -77,5 +63,8 @@ module.exports = {
             default: 0, // here this will by default take the first account as deployer
             1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
         },
+    },
+    mocha: {
+        timeout: 500000,
     },
 }
